a6688046f32a55411af7ac44fdb134a17c091db0
kyla-sutter <257415960+kyla-sutter@users.noreply.github.com>
2026-02-15 15:55:26 -0600

    lab_1_part_1
    My Submission

---
+++ linear_regression/linear_regression_in_cars.py
@@ -0,0 +1,37 @@
+import numpy as np
+import pandas as pd
+import sklearn.linear_model as lm
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+def predict(cars_df: pd.DataFrame) -> None:
+    predictors_df = cars_df[['enginesize']]
+    response_series = cars_df['price'].to_numpy()
+
+    #Train and predict.
+    algorithm = lm.LinearRegression()
+    model = algorithm.fit(predictors_df, response_series)
+    prediction = model.predict(predictors_df)
+    print(response_series)
+    print(prediction)
+    
+     # Calculate r-squared.
+    r_squared = model.score(predictors_df, response_series)
+    print(f"r^2: {r_squared}")
+
+    
+    # Plot Height vs Volume with best fit line
+    plt.figure(figsize=(8, 6))
+    sns.regplot(x=cars_df['enginesize'], y=cars_df['price'])
+    plt.xlabel('Engine Size')
+    plt.ylabel('Price')
+    plt.title('Engine Size vs Price with Best Fit Line')
+    plt.tight_layout()
+    plt.show()
+
+def main():
+    cars_df = pd.read_csv(r"C:\Users\kylas\Downloads\cars.csv")
+    predict(cars_df)
+    
+if __name__ == "__main__":
+    main()
