ea383a1b0f3a133a006d67ab85120342f8d48963
Juancrod03 <jrodriguez50@madisoncollege.edu>
2026-03-23 16:55:47 -0500

    Update LRCars.py
    Lab 2 Update

--- linear_regression/LRCars.py
+++ linear_regression/LRCars.py
@@ -7,7 +7,7 @@ import matplotlib.pyplot as plt
 cars_df = pd.read_csv('linear_regression/cars.csv')
 #print(cars_df.head())
 
-def predict(cars_df):
+def prepare_data(cars_df):
     encoded_cols = pd.get_dummies(cars_df['carbody'])
     cars_df = pd.concat([encoded_cols, cars_df], axis=1)
     columns_to_keep = ['symboling', 'wheelbase', 'carlength', 'carwidth', 'carheight', 
@@ -16,17 +16,29 @@ def predict(cars_df):
         'convertible', 'hardtop', 'hatchback', 'sedan', 'wagon', 
         'price']
     cars_df = cars_df[columns_to_keep]
-    predictors = cars_df.drop(columns=['price'])
-    return predictors
+    return cars_df
 
-predictors_df = predict(cars_df)
-response = cars_df['price']
-#print(predictors_df.head())
+def test_linearity(df):
+    correlations = df.corr()
+    price_corr = correlations[['price']].sort_values(by='price', ascending=False)
+    print(price_corr)
+    
+    strong_predictors = price_corr[abs(price_corr['price']) > 0.5]
+    print(strong_predictors)
+    
+    plt.figure(figsize=(6, 8))
+    sns.heatmap(price_corr, annot=True, cmap='coolwarm', vmin=-1, vmax=1)
+    plt.show()
+
+prepared_df = prepare_data(cars_df)
+test_linearity(prepared_df)
+
+predictors_df = prepared_df.drop(columns=['price'])
+response = prepared_df['price']
 
 lr_model = LinearRegression()
 lr_model.fit(predictors_df, response)
 y_pred = lr_model.predict(predictors_df)
-#print(y_pred)
 r_squared = lr_model.score(predictors_df, response)
 print(f"R-squared: {r_squared}")
 
