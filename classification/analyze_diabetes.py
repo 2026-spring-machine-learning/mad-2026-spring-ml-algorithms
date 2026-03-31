787b91b1ad7e7fbaa903a720a6ea61c4606b5618
chwalk <chwalker@madisoncollege.edu>
2026-03-30 20:16:25 -0500

    Updated diabetes with over-sampling.

--- classification/analyze_diabetes.py
+++ classification/analyze_diabetes.py
@@ -1,6 +1,8 @@
+import numpy as np
 import pandas as pd
 import sklearn.model_selection as ms
 import sklearn.linear_model as lm
+import imblearn.over_sampling as ios
 
 
 def read_diabetes():
@@ -9,41 +11,64 @@ def read_diabetes():
      return df
 
 
+def show_accuracy(diabetes_response_testing, predictions):
+    print(f"Accuracy: {(predictions == diabetes_response_testing.values).mean()}")
+    # print((predictions == diabetes_response_testing.values).mean())
+
+
+def perform_logistic_regression(predictors, response, balance_counter):
+    for random_state in range(0, 3):
+        if balance_counter == 1:
+            random_over_sampler = ios.RandomOverSampler(random_state=random_state)
+            predictors, response \
+                = random_over_sampler.fit_resample(predictors, response)
+
+        (diabetes_predictors_training, diabetes_predictors_testing,
+        diabetes_response_training, diabetes_response_testing) = \
+            ms.train_test_split(predictors, response, test_size=0.2,
+            random_state=random_state) #, random_state=0)
+        # print("Training Predictors:")
+        # print(diabetes_predictors_training)
+        # print("Training Response:")
+        # print(diabetes_response_training)
+        # print("Testing Predictors:")
+        # print(diabetes_predictors_testing)
+        # print("Testing Response:")
+        # print(diabetes_response_testing)
+
+        algorithm = lm.LogisticRegression(max_iter=100000)
+        model = algorithm.fit(diabetes_predictors_training, diabetes_response_training)
+        predictions = model.predict(diabetes_predictors_testing)
+        # print("Predictions:")
+        # print(predictions)
+        # print("Actual:")
+        # print(diabetes_response_testing.values)
+        # print("Comparison:")
+        # print(predictions == diabetes_response_testing.values)
+        # all_zeros = np.zeros_like(diabetes_response_testing)
+        # print(f"all_zeros: {all_zeros}")
+        show_accuracy(diabetes_response_testing, predictions)
+        # show_accuracy(diabetes_response_testing, all_zeros)
+
+
 def analyze(diabetes_df):
     # diabetes_df = ['3']
     diabetes_df = diabetes_df.drop(['DNR Order', 'Med Tech'], axis='columns')
     predictors = diabetes_df.drop('Outcome', axis='columns')
     response = diabetes_df['Outcome']
     # print(predictors.columns)
-    (diabetes_predictors_training, diabetes_predictors_testing,
-     diabetes_response_training, diabetes_response_testing) = \
-        ms.train_test_split(predictors, response, test_size=0.2,
-        random_state=4) #, random_state=0)
-    # print("Training Predictors:")
-    # print(diabetes_predictors_training)
-    # print("Training Response:")
-    # print(diabetes_response_training)
-    # print("Testing Predictors:")
-    # print(diabetes_predictors_testing)
-    # print("Testing Response:")
-    # print(diabetes_response_testing)
 
-    algorithm = lm.LogisticRegression()
-    model = algorithm.fit(diabetes_predictors_training, diabetes_response_training)
-    predictions = model.predict(diabetes_predictors_testing)
-    print("Predictions:")
-    print(predictions)
-    print("Actual:")
-    print(diabetes_response_testing.values)
-    print("Comparison:")
-    print(predictions == diabetes_response_testing.values)
-    print("Accuracy:")
-    print((predictions == diabetes_response_testing.values).mean())
+    for balance_counter in range(2):
+        if balance_counter == 0:
+            print("Unbalanced:")
+        else:
+            print("Balanced:")
+        perform_logistic_regression(predictors, response, balance_counter)
 
 
 def main():
     df = read_diabetes()
-    print(df.head())
+    # print(df.head())
     analyze(df)
 
 
