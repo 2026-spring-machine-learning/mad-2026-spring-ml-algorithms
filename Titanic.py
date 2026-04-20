3bf9aac7df022eb95711088997880b6d3cb45812
Juancrod03 <jrodriguez50@madisoncollege.edu>
2026-04-20 14:58:58 -0500

    Update Titanic.py

--- Titanic.py
+++ Titanic.py
@@ -3,28 +3,40 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import accuracy_score, confusion_matrix
+from imblearn.over_sampling import RandomOverSampler
 
 df = pd.read_csv("Titanic-Dataset.csv")
-cols = ["Survived", "Pclass", "Age", "SibSp", "Parch", "Fare"]
+cols = ["Survived", "Pclass", "Age", "SibSp", "Parch", "Fare", "Sex", "Embarked"]
 df = df[cols]
 
 df = df.dropna()
+
+df["Sex"] = df["Sex"].map({"male" : 0, "female" : 1})
+df = pd.get_dummies(df, columns=["Embarked"], drop_first=True)
 
 #print(df.shape)
 #print(df.head())
 
 print(df.corr(numeric_only=True)["Survived"])
 
-X = df[["Pclass", "Age", "SibSp", "Parch", "Fare"]]
+X = df.drop("Survived", axis=1)
 y = df["Survived"]
 
 X_train, X_test, y_train, y_test = train_test_split(
     X, y, test_size=0.25, random_state=1
 )
 
-model = LogisticRegression(max_iter=1000)
-model.fit(X_train, y_train)
+ros = RandomOverSampler(random_state=1)
+X_train_res, y_train_res = ros.fit_resample(X_train, y_train)
 
+model = LogisticRegression(max_iter=100000)
+model.fit(X_train_res, y_train_res)
+pred = model.predict(X_test)
+
+ros = RandomOverSampler(random_state=1)
+X_train_res, y_train_res = ros.fit_resample(X_train, y_train)
+
+model.fit(X_train_res, y_train_res)
 pred = model.predict(X_test)
 
 print("Predictions:", pred[:20])
