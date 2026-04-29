f9222cbf17a6a9cc2dc7ce2094b4450f05a9f621
Juancrod03 <jrodriguez50@madisoncollege.edu>
2026-04-29 11:44:38 -0500

    Update Titanic.py

--- Titanic.py
+++ Titanic.py
@@ -4,6 +4,8 @@ from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import accuracy_score, confusion_matrix
 from imblearn.over_sampling import RandomOverSampler
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import StratifiedKFold, cross_val_score
 
 df = pd.read_csv("Titanic-Dataset.csv")
 cols = ["Survived", "Pclass", "Age", "SibSp", "Parch", "Fare", "Sex", "Embarked"]
@@ -22,27 +24,35 @@ print(df.corr(numeric_only=True)["Survived"])
 X = df.drop("Survived", axis=1)
 y = df["Survived"]
 
-X_train, X_test, y_train, y_test = train_test_split(
-    X, y, test_size=0.25, random_state=1
-)
+#X_train, X_test, y_train, y_test = train_test_split(
+#    X, y, test_size=0.25, random_state=1
+#)
 
-ros = RandomOverSampler(random_state=1)
-X_train_res, y_train_res = ros.fit_resample(X_train, y_train)
+#ros = RandomOverSampler(random_state=1)
+#X_train_res, y_train_res = ros.fit_resample(X_train, y_train)
 
-model = LogisticRegression(max_iter=100000)
-model.fit(X_train_res, y_train_res)
-pred = model.predict(X_test)
+#model = LogisticRegression(max_iter=100000)
+#model.fit(X_train_res, y_train_res)
+#pred = model.predict(X_test)
 
-ros = RandomOverSampler(random_state=1)
-X_train_res, y_train_res = ros.fit_resample(X_train, y_train)
+model = RandomForestClassifier(n_estimators=100, random_state=1)
+cv = StratifiedKFold(n_splits=10, shuffle=True, random_state=1)
+scores = cross_val_score(model, X, y, cv=cv, scoring="accuracy")
 
-model.fit(X_train_res, y_train_res)
-pred = model.predict(X_test)
+#ros = RandomOverSampler(random_state=1)
+#X_train_res, y_train_res = ros.fit_resample(X_train, y_train)
 
-print("Predictions:", pred[:20])
-print("Accuracy:", accuracy_score(y_test, pred))
-print("Confusion Matrix:\n", confusion_matrix(y_test, pred))
+#model.fit(X_train_res, y_train_res)
+#pred = model.predict(X_test)
 
-all_ones = np.ones_like(y_test)
-print("All 1 accuracy:", accuracy_score(y_test, all_ones))
-print("Actual y_test values:", y_test.values[:20])
+#print("Predictions:", pred[:20])
+#print("Accuracy:", accuracy_score(y_test, pred))
+#print("Confusion Matrix:\n", confusion_matrix(y_test, pred))
+
+#all_ones = np.ones_like(y_test)
+#print("All 1 accuracy:", accuracy_score(y_test, all_ones))
+#print("Actual y_test values:", y_test.values[:20])
+
+print("10-fold accuracy scores:", scores)
+print("Mean accuracy:", scores.mean())
+print("Accuracy standard dev:", scores.std())
